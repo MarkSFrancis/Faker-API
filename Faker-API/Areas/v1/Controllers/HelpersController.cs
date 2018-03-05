6b63ae6f88a53d0b8fef21aee14e2d0052e58f5e
Mark Francis <mrkfrancis8@gmail.com>
2018-03-05 21:39:49 +0000

    Refactored HelpersController

--- Faker-API/Areas/v1/Controllers/HelpersController.cs
+++ Faker-API/Areas/v1/Controllers/HelpersController.cs
@@ -1,4 +1,5 @@
-﻿using Faker_API.Controllers;
+﻿using Bogus;
+using Faker_API.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Faker_API.Areas.v1.Controllers
@@ -6,7 +7,7 @@ namespace Faker_API.Areas.v1.Controllers
     public class HelpersController : BaseApiController
     {
         public IActionResult Replace(string format) =>
-            Result(new Bogus.Randomizer().Replace(format));
+            Result(new Randomizer().Replace(format));
 
         public IActionResult Handlebars(string format) =>
             Result(Faker.Parse(format));
