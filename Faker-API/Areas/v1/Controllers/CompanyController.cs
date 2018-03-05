3749bd57e71ec9ac678f229478b67690bac8da6b
Mark Francis <mrkfrancis8@gmail.com>
2018-03-05 16:09:19 +0000

    Created CompanyController

---
+++ Faker-API/Areas/v1/Controllers/CompanyController.cs
@@ -0,0 +1,27 @@
+﻿using Faker_API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Faker_API.Areas.v1.Controllers
+{
+    public class CompanyController : BaseApiController
+    {
+        public IActionResult CompanySuffix() =>
+            Result(Faker.Company.CompanySuffix());
+
+        public IActionResult CompanyName(string format = null, int? formatIndex = null)
+        {
+            if (format != null)
+            {
+                return Result(Faker.Company.CompanyName(format));
+            }
+
+            return Result(Faker.Company.CompanyName(formatIndex));
+        }
+
+        public IActionResult CatchPhrase() =>
+            Result(Faker.Company.CatchPhrase());
+
+        public IActionResult Bs() =>
+            Result(Faker.Company.Bs());
+    }
+}
