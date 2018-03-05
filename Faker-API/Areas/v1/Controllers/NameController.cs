96e71d92e6c54a26b00d75cf96ed6bf530aee458
Mark Francis <mrkfrancis8@gmail.com>
2018-03-05 20:40:45 +0000

    Added NameController

---
+++ Faker-API/Areas/v1/Controllers/NameController.cs
@@ -0,0 +1,39 @@
+﻿using Bogus.DataSets;
+using Faker_API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Faker_API.Areas.v1.Controllers
+{
+    public class NameController : BaseApiController
+    {
+        public IActionResult FirstName(Name.Gender? gender = null) => 
+            Result(Faker.Name.FirstName(gender));
+        
+        public IActionResult LastName(Name.Gender? gender = null) => 
+            Result(Faker.Name.LastName(gender));
+        
+        public IActionResult FullName(Name.Gender? gender = null) => 
+            Result(Faker.Name.FullName(gender));
+        
+        public IActionResult Prefix(Name.Gender? gender = null) => 
+            Result(Faker.Name.Prefix(gender));
+        
+        public IActionResult Suffix() => 
+            Result(Faker.Name.Suffix());
+        
+        public IActionResult FindName(string firstName = "", string lastName = "", bool? withPrefix = null, bool? withSuffix = null, Name.Gender? gender = null) => 
+            Result(Faker.Name.FindName(firstName, lastName, withPrefix, withSuffix, gender));
+        
+        public IActionResult JobTitle() => 
+            Result(Faker.Name.JobTitle());
+        
+        public IActionResult JobDescriptor() => 
+            Result(Faker.Name.JobDescriptor());
+        
+        public IActionResult JobArea() => 
+            Result(Faker.Name.JobArea());
+        
+        public IActionResult JobType() => 
+            Result(Faker.Name.JobType());
+    }
+}
