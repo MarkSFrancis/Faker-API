dac1374226728d0259997bb5b3686205b398b330
Mark Francis <mrkfrancis8@gmail.com>
2018-03-05 16:56:24 +0000

    Added HackerController

---
+++ Faker-API/Areas/v1/Controllers/HackerController.cs
@@ -0,0 +1,26 @@
+﻿using Faker_API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Faker_API.Areas.v1.Controllers
+{
+    public class HackerController : BaseApiController
+    {
+        public IActionResult Abbreviation() =>
+            Result(Faker.Hacker.Abbreviation());
+
+        public IActionResult Adjective() =>
+            Result(Faker.Hacker.Adjective());
+
+        public IActionResult Noun() =>
+            Result(Faker.Hacker.Noun());
+
+        public IActionResult Verb() =>
+            Result(Faker.Hacker.Verb());
+
+        public IActionResult IngVerb() =>
+            Result(Faker.Hacker.IngVerb());
+
+        public IActionResult Phrase() =>
+            Result(Faker.Hacker.Phrase());
+    }
+}
