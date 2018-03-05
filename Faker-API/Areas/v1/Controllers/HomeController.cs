036e696f0b240cb45306a8a39e8c4f1b3568563f
Mark Francis <mrkfrancis8@gmail.com>
2018-03-05 15:02:05 +0000

    Created base API project

---
+++ Faker-API/Areas/v1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Faker_API.Areas.v1.Controllers
+{
+    [Route("v1/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Json(new
+            {
+                id,
+                page = "Get",
+                success = true
+            });
+        }
+    }
+}
