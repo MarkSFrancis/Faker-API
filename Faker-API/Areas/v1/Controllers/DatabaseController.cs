a931b73a87af54f7a742c33de28c2f228f5f6fdc
Mark Francis <mrkfrancis8@gmail.com>
2018-03-05 16:10:56 +0000

    Added DatabaseController

---
+++ Faker-API/Areas/v1/Controllers/DatabaseController.cs
@@ -0,0 +1,20 @@
+﻿using Faker_API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Faker_API.Areas.v1.Controllers
+{
+    public class DatabaseController : BaseApiController
+    {
+        public IActionResult Column() =>
+            Result(Faker.Database.Column());
+
+        public IActionResult Type() =>
+            Result(Faker.Database.Type());
+
+        public IActionResult Collation() =>
+            Result(Faker.Database.Collation());
+
+        public IActionResult Engine() =>
+            Result(Faker.Database.Engine());
+    }
+}
